6a4532cd0facafdb1e545bcd870fc1fb92523c90
sketchup94 <uemitgul@gmail.com>
2019-06-27 20:11:26 +0200

    RegistrationView added

---
+++ KOFFII/Controller/RegistrationViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RegistrationViewController.swift
+//  KOFFII
+//
+//  Created by Ümit Gül on 27.06.19.
+//  Copyright © 2019 Ümit Gül. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+}
